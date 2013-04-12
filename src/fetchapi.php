f3865a2306b92ca97590b0cdaec56e5f7844a1a9
Valdeck <Valdeck@Valdeck-PC>
2013-04-12 03:28:12 -0500

    moved user folder to src and adjusted path in base

--- src/fetchapi.php
+++ src/fetchapi.php
@@ -1,8 +1,9 @@
+<?php
 function fetchData($sql){
 	 $ch = curl_init(); 
 
     // set url 
-    curl_setopt($ch, CURLOPT_URL, "http://data.caribbeanopeninstitute.org/api/action/datastore_search_sql?sql="+$sql); 
+    curl_setopt($ch, CURLOPT_URL, "http://data.caribbeanopeninstitute.org/api/action/datastore_search?resource_id=57aba0a9-d958-4a2e-85c2-47a2e2c9e6be&limit=5"); 
 
     //return the transfer as a string 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
@@ -18,8 +19,9 @@ function fetchData($sql){
     	$out = json_encode($output["records"]);
     }
     else {
-    	$out = json_encode(array("failed"=>1))
+    	$out = json_encode(array("failed"=>1));
     }
-    return $out;
+    echo $out;
 }
-
+fetchData("ghj");
+?>
